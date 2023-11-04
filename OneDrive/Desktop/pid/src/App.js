dcc272b3f94c0e99532b3035e71f0882036f5f2a
keyur-doshi <keyurdoshi03@gmail.com>
2023-11-04 11:32:30 +0530

    First commit

---
+++ OneDrive/Desktop/pid/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header/Header';
+import Rules from './Rules/Rules';
+import RegistrationNumberComponent from './RegNum/RegistrationNumberComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Rules/>
+      <RegistrationNumberComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ OneDrive/Desktop/pid/src/Rules/Rules.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Rules.css';
+
+function Rules() {
+  const rules = [
+    "Participants can register anytime before the event ends. The topics will be released only when the event starts.",
+    "Participants should submit their works during the event timeline only, and strictly in a pdf format.",
+    "Participants can submit their work anytime during the event submission window, which is open from 2 pm to 6 pm.",
+    "There is no word limit to the stories. They can be as long or as short as you want!",
+    "The aforementioned submission pdf must contain the name of the participant, registration number, and topic mandatorily, or else the submission won't be considered.",
+    "Plagiarism and AI checks will be kept in place, and any offenders will be disqualified from the competition.",
+    "Profanities in the written work will not be tolerated."
+  ];
+  
+  const googleFormLink = "https://forms.gle/EZzqTLGCSki9iwBg8";
+
+  return (
+    <div className="rules-container">
+      <h2>Event Rules</h2>
+      <p className="centered-paragraph">
+        All participants are strictly required to adhere to the following rules. After reading the rules, you are required to enter your registration number to get your topic. The writing piece has to be submitted using the Google form link given at the end. All the best.<br/> <b>Happy Writing!</b>
+      </p>
+      <ul>
+        {rules.map((rule, index) => (
+          <li key={index}>{rule}</li>
+        ))}
+      </ul>
+      <p className="centered-paragraph">
+        <b> <a href={googleFormLink} target="_blank" rel="noopener noreferrer">Submit your work here</a> </b>
+      </p>
+    </div>
+  );
+}
+
+export default Rules;
